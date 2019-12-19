ebdf9d30cdcf8154009b0a54164220218e30092e
RemiM <rmelisson@gmail.com>
2019-12-19 13:22:52 +0100

    add dump script

---
+++ dump-matomo-yesterday.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+date=`date --date="-1 day" "+%Y-%m-%d"`
+
+url="https://matomo.tools.factory.social.gouv.fr/index.php?module=API&method=Live.getLastVisitsDetails&idSite=4&period=day&date=$date&format=JSON&token_auth=anonymous&filter_limit=-1"
+
+name=matomo-dump-$date
+file=$name.json
+
+echo "Download Matomo content for $date"
+curl $url -o $file
+
+
+echo "Push file to Azure"
+az storage blob upload \
+  --account-key "$AZ_STORAGE_TOKEN" \
+  --account-name=cdtndata \
+  --container data \
+  --file $file \
+  --name $name
+
+
+echo "Delete file"
+rm $file
