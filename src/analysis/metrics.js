686173513774fa78617eb9a153cbfb653d60133b
ArmandGiraud <armand.giraud.ag@gmail.com>
2020-07-15 11:33:19 +0200

    modify params

--- src/analysis/metrics.js
+++ src/analysis/metrics.js
@@ -20,20 +20,51 @@ const typeCounts = (visit) => {
         };
     })
 };
+const filterNas = array => array.filter(a => a != null);
+const add = (a, b) => a + b;
 
+const sumFilter = array => {
+  const filteredArray = filterNas(array);
+  return filteredArray.reduce(add);
+};
+const avg = array => {
+    const sum = array.reduce((previous, current) => (current += previous));
+    return sum / array.length;
+  };
+const isLongVisit = (x) => {
+    if(x.type == "visit_content") {
+        return x.count > 2
+    }else{
+        return false
+    };
+};
+
+const getUserType = (visitCount) => {
+    const longvisits = visitCount.map(x => isLongVisit(x))
+    return (sumFilter(longvisits))
+
+}
 const analyse = (dataset, reportId) => {
-    const visits = datasetUtil.getVisits(dataset)//series of dataframes
+    const visits = datasetUtil.getVisits(dataset) //series of dataframes
     //console.log(visits.toArray()[30].toArray()) 
     const seriesOfVisits = visits.toArray()
     // clean urls
     const cleanSeriesOfVisits = seriesOfVisits.map(x => x.transformSeries({
         url: (u) => util.urlToPath(removeQuery(removeAnchor(u))),
     }))
-    // deduplicates urls within user session
+    // deduplicates urls within user session (reloads)
 
     //console.log(uniqueSeriesOfVisits[30].toArray())
     const uniqueSeriesOfVisits = cleanSeriesOfVisits.map(x => x.distinct(x => x.url))
-    console.log(uniqueSeriesOfVisits.map(visit => typeCounts(visit).toArray()))
+    // count visits by type of events (give a list of dict [{'type':visit_content, 'count': 2}, {}])
+    const visitsTypesCount = uniqueSeriesOfVisits.map(visit => typeCounts(visit))
+    const longVisitNb = visitsTypesCount.map(x => getUserType(x.toArray()))
+
+
+    const LongVisitRatio = avg(longVisitNb)
+    //console.log(visitsTypesCount[0])
+    console.log()
+
     return dataset
 }
 
