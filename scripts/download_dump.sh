0409b6c052fb22c696bb8b61cc1d350687e38ad6
RemiM <rmelisson@gmail.com>
2020-12-15 11:36:01 +0100

    chore: fix another date issue

--- scripts/download_dump.sh
+++ scripts/download_dump.sh
@@ -2,8 +2,8 @@
 
 mkdir data
 
-#date=`date -d "@$(($(date +%s) - 86400))"  "+%Y-%m-%d"`
-date=2020-09-30
+date=`date -d "@$(($(date +%s) - 86400))"  "+%Y-%m-%d"`
+#date=2020-09-30
 
 az storage blob download \
   --account-key "$AZ_STORAGE_TOKEN" \
