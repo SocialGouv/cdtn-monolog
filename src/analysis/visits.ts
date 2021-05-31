47c1a396d10bec805cfe406c7f6600d0abdc36d3
ArmandGiraud <armand.giraud@jobteaser.com>
2021-05-31 17:19:45 +0200

    weekdays

--- src/analysis/visits.ts
+++ src/analysis/visits.ts
@@ -4,10 +4,14 @@ import { parseISO } from "date-fns";
 import { MonthlyReport } from "./reports";
 
 const reportType = "monthly-report";
-
+const weekEndNumbers = [0, 6];
 const analyse = (dataset: IDataFrame, reportId: string): MonthlyReport => {
-  const counts = dataset.getSeries("count");
-  const dates = dataset.getSeries("day").select((d) => parseISO(d));
+  const datasetFiltered = dataset.where(
+    (d) => !weekEndNumbers.includes(parseISO(d.day).getDay()) // remove weekends
+  );
+  console.log(datasetFiltered.toArray());
+  const counts = datasetFiltered.getSeries("count");
+  const dates = datasetFiltered.getSeries("day").select((d) => parseISO(d));
 
   // logs start at 8am
   const min = new Date(dates.min());
