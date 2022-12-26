b1cace459b6ccc08a7a69440b1d3d051260ff135
Fabien Roussel <fabien.roussel921@gmail.com>
2022-12-26 14:22:37 +0100

    feat: ajout de l'exploration de données au git

---
+++ explorations/join_multiple_file.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import glob
+
+
+# Path to monolog
+PATH_CDTN_MONOLOG = "/Users/fabien.roussel/Documents/Missions/fabrik-social/cdtn/cdtn-monolog"
+
+
+def pd_read_pattern(pattern):
+    files = glob.glob(pattern)
+
+    list_of_df = []
+    for f in files:
+        list_of_df.append(pd.read_csv(f))
+        
+    df = pd.concat(list_of_df)
+    return df.reset_index(drop=True)
+
+def get_df_from_pattern_and_save_to_csv(pattern, filename='big_file.csv'):
+    df = pd_read_pattern(pattern)
+    df.to_csv(filename)
+    return df
+    
