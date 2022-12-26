e743fa0fafd49d16d6f5456a409a562484dbeb03
Fabien Roussel <fabien.roussel921@gmail.com>
2022-12-26 14:22:37 +0100

    feat: ajout de l'exploration de données au git

---
+++ explorations/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cdtn-explorations',
+    version='0.1.1',
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    author='OCTO Technology',
+    install_requires=['elasticsearch==7.17',
+                      #'eland==8.3', with elasticsearch 8.3 for explo 7
+                      'jupyterlab',
+                      'notebook==6.4',
+                      'pandas==1.5',
+                      'plotly-express==0.4',
+                      'psycopg2-binary==2.9',
+                      'python-dotenv==0.20',
+                      'requests==2.28',
+                      'sentence-transformers==2.2',
+                      'sqlalchemy==1.4',
+                      'torch==1.12',
+                      'transformers==4.21',
+                      'tqdm==4.64',
+                      'unidecode==1.3.4'],
+    python_requires='>=3.9',
+)
