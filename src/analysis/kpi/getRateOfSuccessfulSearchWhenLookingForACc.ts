4228853f88b3a3473b422a2ed3c8bee9aee0bdd8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-14 18:14:36 +0100

    fix: add graph

--- src/analysis/kpi/getRateOfSuccessfulSearchWhenLookingForACc.ts
+++ src/analysis/kpi/getRateOfSuccessfulSearchWhenLookingForACc.ts
@@ -24,24 +24,13 @@ export const getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates = (
   };
 };
 
-export const getRateOfSuccessfulSearchesWhenLookingForACcInCertainTools = (
-  logs: IDataFrame
-): KpiRate => {
-  return getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(
-    logs,
-    "cc_search",
-    "cc_select_p1"
-  );
+export const getRateOfSuccessfulSearchesWhenLookingForACcInCertainTools = (logs: IDataFrame): KpiRate => {
+  return getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(logs, "cc_search", "cc_select_p1");
 };
 
-export const getRateOfSuccessfulSearchesWhenLookingForAnEnterpriseInCertainTools =
-  (logs: IDataFrame): KpiRate => {
-    return getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(
-      logs,
-      "enterprise_search",
-      "enterprise_select"
-    );
-  };
+export const getRateOfSuccessfulSearchesWhenLookingForAnEnterpriseInCertainTools = (logs: IDataFrame): KpiRate => {
+  return getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(logs, "enterprise_search", "enterprise_select");
+};
 
 /**
  * This function compute the rate of successful searches (when clicking on a result) for visitors searching for a cc or an enterprise in
@@ -51,44 +40,48 @@ export const getRateOfSuccessfulSearchesWhenLookingForAnEnterpriseInCertainTools
  * @param {Date} startDate - the first day of the month using to order the report in kibana
  * @param {string} reportId - the timestamp in string of the date where the script is running
  **/
-export const getRatesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools =
-  (logs: IDataFrame, startDate: Date, reportId: string): KpiReport[] => {
-    const logsTools = logs
-      .filter(
-        (log) =>
-          log.url != undefined &&
-          (log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-demission") ||
-            log.url.startsWith("https://code.travail.gouv.fr/outils/indemnite-precarite") ||
-            log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-licenciement") ||
-            log.url.startsWith("https://code.travail.gouv.fr/outils/heures-recherche-emploi") ||
-            log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-retraite") ||
-            log.url.startsWith("https://code.travail.gouv.fr/outils/convention-collective"))
-      )
-      .bake();
-    const rateOfSuccessfulSearchWhenLookingForACcInCertainTools =
-      getRateOfSuccessfulSearchesWhenLookingForACcInCertainTools(logsTools);
-    const rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools =
-      getRateOfSuccessfulSearchesWhenLookingForAnEnterpriseInCertainTools(logsTools);
+export const getRatesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools = (
+  logs: IDataFrame,
+  startDate: Date,
+  reportId: string
+): KpiReport[] => {
+  const logsTools = logs
+    .filter(
+      (log) =>
+        log.url != undefined &&
+        (log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-demission") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/indemnite-precarite") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-licenciement") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/heures-recherche-emploi") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/preavis-retraite") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/convention-collective") ||
+          log.url.startsWith("https://code.travail.gouv.fr/outils/indemnite-licenciement"))
+    )
+    .bake();
+  const rateOfSuccessfulSearchWhenLookingForACcInCertainTools =
+    getRateOfSuccessfulSearchesWhenLookingForACcInCertainTools(logsTools);
+  const rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools =
+    getRateOfSuccessfulSearchesWhenLookingForAnEnterpriseInCertainTools(logsTools);
 
-    return [
-      formatKpiReport(
-        rateOfSuccessfulSearchWhenLookingForACcInCertainTools.denominator,
-        "Rate-of-successful-results-when-searching-cc-in-tools",
-        rateOfSuccessfulSearchWhenLookingForACcInCertainTools.numerator,
-        reportId,
-        startDate,
-        "Recherches de CC dans les outils"
-      ),
-      formatKpiReport(
-        rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools.denominator,
-        "Rate-of-successful-results-when-searching-enterprise-in-tools",
-        rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools.numerator,
-        reportId,
-        startDate,
-        "Recherches d'entreprises dans les outils"
-      ),
-    ];
-  };
+  return [
+    formatKpiReport(
+      rateOfSuccessfulSearchWhenLookingForACcInCertainTools.denominator,
+      "Rate-of-successful-results-when-searching-cc-in-tools",
+      rateOfSuccessfulSearchWhenLookingForACcInCertainTools.numerator,
+      reportId,
+      startDate,
+      "Recherches de CC dans les outils"
+    ),
+    formatKpiReport(
+      rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools.denominator,
+      "Rate-of-successful-results-when-searching-enterprise-in-tools",
+      rateOfSuccessfulSearchWhenLookingForAnEnterpriseInCertainTools.numerator,
+      reportId,
+      startDate,
+      "Recherches d'entreprises dans les outils"
+    ),
+  ];
+};
 
 /**
  * This function compute the rate of successful searches (when clicking on a result) for visitors searching for a cc in
@@ -103,17 +96,13 @@ export const getRateOfSuccessfulSearchesWhenLookingForACcInContribPages = (
   startDate: Date,
   reportId: string
 ): KpiReport => {
-  const logsContrib = filterDataframeByUrlWithPrefix(
-    logs,
-    "https://code.travail.gouv.fr/contribution/"
-  ).bake();
+  const logsContrib = filterDataframeByUrlWithPrefix(logs, "https://code.travail.gouv.fr/contribution/").bake();
 
-  const rateOfSuccessfulSearches =
-    getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(
-      logsContrib,
-      "cc_search",
-      "cc_select"
-    );
+  const rateOfSuccessfulSearches = getRateOfGivenTypeOverAnOtherInDfWithoutDuplicates(
+    logsContrib,
+    "cc_search",
+    "cc_select"
+  );
 
   return formatKpiReport(
     rateOfSuccessfulSearches.denominator,
@@ -140,33 +129,19 @@ export const getRateOfSuccessfulSearchWhenLookingForACc = (
 ): KpiReport[] => {
   // We reduce the size of the dataframe in order to remove anchor more efficiently
   const logsWithTypeSelectOrSearch = logs.filter((log) =>
-    [
-      "cc_search",
-      "cc_select",
-      "cc_select_p1",
-      "enterprise_search",
-      "enterprise_select",
-    ].includes(log.type)
+    ["cc_search", "cc_select", "cc_select_p1", "enterprise_search", "enterprise_select"].includes(log.type)
   );
   const logsWithUrlCleaned = removeAnchorFromUrl(logsWithTypeSelectOrSearch);
 
   //const subsetLogs = logs.subset(["url", "type", "idVisit"]);
 
   const rateOfSuccessfulSearchesWhenLookingForACcInContribPages =
-    getRateOfSuccessfulSearchesWhenLookingForACcInContribPages(
-      logsWithUrlCleaned,
-      startDate,
-      reportId
-    );
+    getRateOfSuccessfulSearchesWhenLookingForACcInContribPages(logsWithUrlCleaned, startDate, reportId);
 
   const ratesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools =
-    getRatesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools(
-      logsWithUrlCleaned,
-      startDate,
-      reportId
-    );
+    getRatesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools(logsWithUrlCleaned, startDate, reportId);
 
-  return ratesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools.concat(
-    [rateOfSuccessfulSearchesWhenLookingForACcInContribPages]
-  );
+  return ratesOfSuccessfulSearchesWhenLookingForACcOrAnEnterpriseInCertainTools.concat([
+    rateOfSuccessfulSearchesWhenLookingForACcInContribPages,
+  ]);
 };
