f0136224918892a689fd899180dec3f42014f002
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-14 12:12:55 +0100

    fix: tu

---
+++ scripts/download_dump_precise_date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir data
+
+date=$1
+
+az storage blob download \
+  --account-key "$AZ_STORAGE_TOKEN" \
+  --account-name=cdtndata \
+  --container logs \
+  --name matomo-dump-$date \
+  --file data/$date.json
